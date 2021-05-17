c695d022c97b41a94bcc3d537806a6db2f27fe60
Sergey Kurdyukov <sergey.kurdyukov.v@gmail.com>
2021-05-17 22:49:42 +0300

    Added My articles page

--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -5,7 +5,7 @@ const {Router} = require(`express`);
 const router = new Router();
 
 router.get(`/`, (req, res) => {
-  res.send(`/my`);
+  res.render(`my`);
 });
 
 router.get(`/comments`, (req, res) => {
