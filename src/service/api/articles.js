fbbffdf07c067057ec204b165b2c6514c4068666
Sergey Kurdyukov <sergey.kurdyukov.v@gmail.com>
2021-05-25 17:09:37 +0300

    Get article by id logic added

--- src/service/api/articles.js
+++ src/service/api/articles.js
@@ -13,4 +13,13 @@ module.exports = (app, service) => {
     const articles = service.findAll();
     res.status(HttpCodes.OK).json(articles);
   });
+
+  route.get(`/:id`, (req, res) => {
+    const article = service.findOne(req.params.id);
+    if (!article) {
+      return res.status(HttpCodes.NOT_FOUND).send(`The article with id: '${req.params.id}' does not found`);
+    }
+    res.status(HttpCodes.OK).json(article);
+  });
+
 };
