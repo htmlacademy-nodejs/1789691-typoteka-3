d16a25429d2d9bd828b4d6b5b6d7e41207e8cfad
Sergey Kurdyukov <sergey.kurdyukov.v@gmail.com>
2021-05-26 14:52:22 +0300

    Service parameter renamed

--- src/service/api/articles.js
+++ src/service/api/articles.js
@@ -8,35 +8,35 @@ const articleExists = require(`../middlewares/article-exists`);
 
 const route = new Router();
 
-module.exports = (app, service) => {
+module.exports = (app, articleService) => {
   app.use(`/articles`, route);
 
   route.get(`/`, (req, res) => {
-    const articles = service.findAll();
+    const articles = articleService.findAll();
     res.status(HttpCodes.OK).json(articles);
   });
 
-  route.get(`/:id`, articleExists(service), (req, res) => {
-    const article = service.findOne(req.params.id);
+  route.get(`/:id`, articleExists(articleService), (req, res) => {
+    const article = articleService.findOne(req.params.id);
     res.status(HttpCodes.OK).json(article);
   });
 
   route.post(`/`, articleValidator, (req, res) => {
-    const article = service.create(req.body);
+    const article = articleService.create(req.body);
     res.status(HttpCodes.CREATED).json(article);
   });
 
-  route.put(`/:id`, [articleValidator, articleExists(service)], (req, res) => {
-    const article = service.update(req.params.id, req.body);
+  route.put(`/:id`, [articleValidator, articleExists(articleService)], (req, res) => {
+    const article = articleService.update(req.params.id, req.body);
     res.status(HttpCodes.OK).send(`Updated`);
   });
 
-  route.delete(`/:id`, articleExists(service), (req, res) => {
-    const article = service.delete(req.params.id);
+  route.delete(`/:id`, articleExists(articleService), (req, res) => {
+    const article = articleService.delete(req.params.id);
     res.status(HttpCodes.OK).json(article);
   });
 
-  route.get(`/:id/comments`, articleExists(service), (req, res) => {
+  route.get(`/:id/comments`, articleExists(articleService), (req, res) => {
     const {article} = res.locals;
     res.status(HttpCodes.OK).json(article.comments);
   });
