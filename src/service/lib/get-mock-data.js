8e174acceb324406b4224494b1cce8c6f2e98154
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2022-07-27 20:40:04 +0300

    Added the method to read mock data synchronously

--- src/service/lib/get-mock-data.js
+++ src/service/lib/get-mock-data.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require(`fs/promises`);
+const fsCommon = require(`fs`);
 const chalk = require(`chalk`);
 const {FILE_NAME} = require(`../../../constants`);
 let data = null;
@@ -20,4 +21,22 @@ const getMockData = async () => {
   }
 };
 
-module.exports = getMockData;
+const getMockDataSync = () => {
+  if (data !== null) {
+    return data;
+  }
+
+  try {
+    const content = fsCommon.readFileSync(FILE_NAME);
+    data = JSON.parse(content);
+    return data;
+  } catch (error) {
+    console.error(chalk.red(`Cannot get mocks synchronously. Error:`), error);
+    return error;
+  }
+};
+
+module.exports = {
+  getMockData,
+  getMockDataSync,
+}
