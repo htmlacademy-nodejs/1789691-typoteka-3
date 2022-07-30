22c5f929de97d75542fc582ef0e79aa69d3866b6
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2022-07-30 12:42:53 +0300

    validation logic has been moved into a separated function

--- src/service/middlewares/article-validator.js
+++ src/service/middlewares/article-validator.js
@@ -4,11 +4,14 @@ const {HttpCodes} = require(`../../../constants`);
 
 const articleKeys = [`title`, `announce`, `fullText`, `category`];
 
-const articleValidator = (req, res, next) => {
-  const newArticleKeys = Object.keys(req.body);
-  const isKeyExist = articleKeys.every((key) => newArticleKeys.includes(key));
-  if (!isKeyExist) {
-    return res.status(HttpCodes.BAD_REQUEST).send(`Bad request`);
+const isArticleValid = (reqBody) => {
+  const newArticleKeys = Object.keys(reqBody);
+  return newArticleKeys.every((key) => articleKeys.includes(key));  
+}
+
+const articleValidator = (req, res, next) => {  
+  if (!isArticleValid(req.body)) {
+    return res.status(HttpCodes.BAD_REQUEST).send(`Probably, the request contains an invalid field`);
   }
 
   return next();
