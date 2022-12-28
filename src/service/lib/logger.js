5d2c126ffe654a81f0f0b79a4d2c6611b9c916ac
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2022-12-28 18:51:13 +0300

    Direct the log output according to the NODE_ENV variable value

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,13 +1,14 @@
 'use strict';
 
+const { Environments } = require('../../../constants')
 const pino = require(`pino`)
 const logger = pino({
     name: `pino-and-express`,
     level: process.env.LOG_LEVEL || `info`
   },
-  pino.destination({
-    dest: './logs/pino.log'
-  })
+  process.env.NODE_ENV === Environments.DEVELOPMENT
+    ? process.stdout
+    : pino.destination({ dest: './logs/pino.log' })
 );
 
 module.exports = {
