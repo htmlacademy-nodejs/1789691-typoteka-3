f09370855536b0b6923ca7d818ec8fb30ab32ce1
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2023-01-02 19:37:34 +0300

    Show real data on the main page

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -3,11 +3,16 @@
 const { defaultApi } = require('../api');
 const {Router} = require(`express`);
 
+const images = [
+  '/img/skyscraper@1x.jpg',
+  '/img/sea@1x.jpg',
+  '/img/forest@1x.jpg'
+]
 
 const router = new Router();
 router.get(`/`, async (req, res) => {
   const articles = await defaultApi.getArticles();
-  res.render(`main`)
+  res.render(`main`, { articles, images })
 });
 
 module.exports = router;
