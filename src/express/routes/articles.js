64e8ff765385114f71b2f3ded8ce2905c189cdc3
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2023-01-03 21:37:27 +0300

    Added changes to post form data with an uploading file

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -2,12 +2,17 @@
 
 const { defaultApi } = require('../api');
 const {Router} = require(`express`);
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const router = new Router();
 
 router.get(`/add`, (req, res) => {
   res.render(`new-post`);
+});
+
+router.post(`/add`, upload.single('upload'), (req, res) => {
+  res.send(`Body: ${JSON.stringify(req.body)}.<br> File: ${JSON.stringify(req.file)}`);
 });
 
 router.get(`/edit/:id`, async (req, res) => {
