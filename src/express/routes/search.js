ff8037187936e8490edf9145ca14a4fc15dcbecb
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2023-01-04 00:44:36 +0300

    Show search reasults

--- src/express/routes/search.js
+++ src/express/routes/search.js
@@ -1,11 +1,17 @@
 'use strict';
 
+const { defaultApi } = require('../api');
 const {Router} = require(`express`);
 
 const router = new Router();
 
 router.get(`/`, (req, res) => {
   res.render(`search`);
+});
+
+router.post(`/`, async (req, res) => {
+  const articles = await defaultApi.searchArctile(req.body.search);
+  res.render(`search`, { articles });
 });
 
 module.exports = router;
